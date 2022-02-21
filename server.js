7f582da85cfb293355beae694f914e80c7ec6f98
Tsara Sudrajat <tsara.tf@gmail.com>
2022-02-21 17:22:12 +0700

    Add upload and resturn json

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +13,14 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res) {
+  console.log(req.file);
+  res.json({
+    name: req.file.originalname,
+    size: req.file.size,
+    type: req.file.mimetype
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
